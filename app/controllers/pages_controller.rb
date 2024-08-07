e192129b392fb320ce8533920563f8763a1f1a0d
alexohre <alexokomado@gmail.com>
2024-08-07 16:05:05 +0100

    i think am done
    Signed-off-by: alexohre <alexokomado@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
-  # before_action :authenticate_user!
-  layout "auth", only: [:login]
+  before_action :authenticate_quizmaster, only: [:quizmaster]
+  before_action :authenticate_judges, only: [:judges]
+  before_action :authenticate_timer, only: [:timer]
+  before_action :authenticate_scoreboard, only: [:scoreboard]
+  before_action :authenticate_recorder, only: [:recorder]
+  
   def home
   end
 
-  def login
+  def recorder
   end
 
   def scoreboard
@@ -19,4 +23,37 @@ class PagesController < ApplicationController
   def timer
     
   end
+
+  private
+
+  def authenticate_quizmaster
+    unless current_user&.admin? || current_user&.quiz_master?
+      redirect_to "/404"
+    end
+  end
+
+  def authenticate_judges
+    unless current_user&.admin? || current_user&.judges?
+      redirect_to "/404"
+    end
+  end
+
+  def authenticate_timer
+    unless current_user&.admin? || current_user&.time_keeper?
+      redirect_to "/404"
+    end
+  end
+
+  def authenticate_scoreboard
+    unless current_user&.admin? || current_user&.presenter? || current_user&.recorder?
+      redirect_to "/404"
+    end
+  end
+
+  def authenticate_recorder
+    unless current_user&.admin? || current_user&.recorder?
+      redirect_to "/404"
+    end
+  end
+
 end
